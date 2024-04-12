ff503bdac46c0534f40f465c6c3cf3771b932d97
lwitcher <lwitcher@163.com>
2024-04-12 23:58:56 +0800

    upd

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Carrot, BookMedical,Tools,PenNib } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Carrot, BookMedical,Tools,PenNib,Database } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -73,6 +73,8 @@ const siteIcon = {
   Carrot,
   BookMedical,
   Tools,
+  PenNib,
+  Database,
 };
 
 // 链接跳转
